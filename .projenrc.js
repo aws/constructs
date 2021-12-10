e591bcf3bf6efa73975e9e3c1a19b402bfcd40ec
Elad Ben-Israel <benisrae@amazon.com>
2021-12-10 07:11:20 +0200

    change npmDistTag of main branch to latest-3

--- .projenrc.js
+++ .projenrc.js
@@ -8,6 +8,8 @@ const project = new cdk.JsiiProject({
   // release branches
   defaultReleaseBranch: 'main',
   majorVersion: 3,
+  npmDistTag: 'latest-3',
+  
   releaseBranches: {
     '10.x': { majorVersion: 10 },
   },
