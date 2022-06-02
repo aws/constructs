53910d9810a58035aada3fa7a32454b048f6e6c3
Thomas Steinbach <ThomasSteinbach@users.noreply.github.com>
2022-06-02 16:45:03 +0200

    docs: Renamed 'DependableTrait' to new name 'Dependable'
    It looks like the former Name of 'Dependable' was 'DependableTrait'. Latter name is mentioned in the code documentation, which doesn't fit to the code anymore. I updated the docs regarding the current code.

--- src/dependency.ts
+++ src/dependency.ts
@@ -4,7 +4,7 @@ import { IConstruct } from './construct';
  * Trait marker for classes that can be depended upon
  *
  * The presence of this interface indicates that an object has
- * an `IDependableTrait` implementation.
+ * an `Dependable` implementation.
  *
  * This interface can be used to take an (ordering) dependency on a set of
  * constructs. An ordering dependency implies that the resources represented by
@@ -64,11 +64,11 @@ const DEPENDABLE_SYMBOL = Symbol.for('@aws-cdk/core.DependableTrait');
  * @example
  *
  * // Usage
- * const roots = DependableTrait.get(construct).dependencyRoots;
+ * const roots = Dependable.of(construct).dependencyRoots;
  *
  * // Definition
- * DependableTrait.implement(construct, {
- *   get dependencyRoots() { return []; }
+ * Dependable.implement(construct, {
+ *       dependencyRoots: [construct],
  * });
  *
  * @experimental
