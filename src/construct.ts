0fd9720171328be6057f001b2e47bf887dd398b4
awslukeguan <150387335+awslukeguan@users.noreply.github.com>
2024-08-07 12:55:35 -0400

    chore: updated the documentation on addMetadata (#2371)
    Clarify the understanding that the addMetadata is writing to the construct tree, visible in the cloud assembly of the manifest.json. Not visible in the Cloudformation template.

--- src/construct.ts
+++ src/construct.ts
@@ -283,6 +283,8 @@ export class Node {
    * Entries are arbitrary values and will also include a stack trace to allow tracing back to
    * the code location for when the entry was added. It can be used, for example, to include source
    * mapping in CloudFormation templates to improve diagnostics.
+   * Note that construct metadata is not the same as CloudFormation resource metadata and is never written to the CloudFormation template.
+   * The metadata entries are written to the Cloud Assembly Manifest if the `treeMetadata` property is specified in the props of the App that contains this Construct.
    *
    * @param type a string denoting the type of metadata
    * @param data the value of the metadata (can be a Token). If null/undefined, metadata will not be added.
