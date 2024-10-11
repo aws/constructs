0b6c1c5165e0888546f981f76719916f8e2dcc20
Momo Kornher <mail@moritzkornher.de>
2024-10-11 20:18:56 +0100

    keep typo fixes

--- src/construct.ts
+++ src/construct.ts
@@ -95,9 +95,9 @@ export class Node {
    * Addresses are calculated using a SHA-1 of the components of the construct
    * path.
    *
-   * To enable refactorings of construct trees, constructs with the ID `Default`
+   * To enable refactoring of construct trees, constructs with the ID `Default`
    * will be excluded from the calculation. In those cases constructs in the
-   * same tree may have the same addreess.
+   * same tree may have the same address.
    *
    * @example c83a2846e506bcc5f10682b564084bca2d275709ee
    */
