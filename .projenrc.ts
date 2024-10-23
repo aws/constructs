dfc0e1b8c7b827403cbe142ad5be0f66f694338c
cdklabs-automation <90142015+cdklabs-automation@users.noreply.github.com>
2024-10-23 05:26:05 -0700

    chore(ci): switch to merge queues (#2523)
    * chore(deps): upgrade cdklabs-projen-project-types
    
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/aws/constructs/actions/runs/11460986971
    
    ------
    
    *Automatically created by projen via the "upgrade-cdklabs-projen-project-types-3.x" workflow*
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    * merge queues
    
    * try again
    
    * try again
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Momo Kornher <kornherm@amazon.co.uk>

--- .projenrc.ts
+++ .projenrc.ts
@@ -61,6 +61,7 @@ const project = new CdklabsJsiiProject({
   minNodeVersion: '16.14.0',
   workflowNodeVersion: '18.x',
 
+  enablePRAutoMerge: true,
   compat: true,
   autoApproveOptions: {
     allowedUsernames: ['cdklabs-automation'],
