590943a89a1883208fa74cb9efabca8391280264
Tietew <tietew@gmail.com>
2024-12-31 00:29:54 +0900

    chore: fix a minor typo in doc (#2580)
    * fix a minor typo in doc
    
    * docs
    
    ---------
    
    Co-authored-by: Momo Kornher <kornherm@amazon.co.uk>

--- src/construct.ts
+++ src/construct.ts
@@ -136,7 +136,7 @@ export class Node {
   }
 
   /**
-   * Returns the child construct that has the id `Default` or `Resource"`.
+   * Returns the child construct that has the id `Default` or `Resource`.
    * This is usually the construct that provides the bulk of the underlying functionality.
    * Useful for modifications of the underlying construct that are not available at the higher levels.
    *
